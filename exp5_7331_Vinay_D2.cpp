5ce5c9b729cebe31ee9ea8c8d84aaab0fa5957fb
vinay7331 <163515163+vinay7331@users.noreply.github.com>
2024-03-27 12:50:11 +0530

    Update exp5_7331_Vinay_D2.cpp

--- exp5_7331_Vinay_D2.cpp
+++ exp5_7331_Vinay_D2.cpp
@@ -1,3 +1,12 @@
+// Classes are mainly used for wrapping a data and functions into a single unit. These are the main feature of
+object-oriented programming. Whenever we need to encapsulate the data and the logic for processing the
+data. To maintain clarity, you decide to define member functions outside the class declaration. Explain how
+you would use the scope resolution operator to define the functions outside the class and ensure proper
+linkage with the help of c++ programming example.
+
+
+
+
 #include<iostream>
 using namespace std;
 class sum{
